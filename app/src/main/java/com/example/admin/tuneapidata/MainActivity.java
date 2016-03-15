b880869ac080b5b274cf31171aaf2ec91d318c29
ramasamy11 <ramasamy.vcp@gmail.com>
2016-03-15 16:13:37 +0530

    add sdk jar to app

--- app/src/main/java/com/example/admin/tuneapidata/MainActivity.java
+++ app/src/main/java/com/example/admin/tuneapidata/MainActivity.java
@@ -11,16 +11,15 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     NetworkDatabase networkDatabase;
-    TextView  mTextView1;
-    private static String URL = "https://api.mobileapptracking.com/v2/advertiser/stats/installs/count.json?start_date=2016-03-01+00:00:00+Indian/" +
-            "Chagos&end_date=2016-03-05+00:00:00+Indian/Chagos&session_token=c38f4585a26c1777e238d9ea571f9c381bfd6222&session_token=f47ff326482520410909fce4f841dd6b85384aea";
+    TextView  mTextView1,mTextView2,mTextView3,mTextView4;
+    private static String URL = "https://api.mobileapptracking.com/v2/advertiser/stats/installs/find?sorts=" +
+            "created+asc&fields=created,site&start_date=2016-01-06&end_date=2016-03-15&limit=1&api_key=25979496beb9bcb7dec023654f91f920";
     private RequestQueue requestQueue;
 
     @Override
@@ -29,32 +28,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         networkDatabase = new NetworkDatabase(getApplication());
         mTextView1 = (TextView)findViewById(R.id.record1);
+        mTextView2 = (TextView)findViewById(R.id.record2);
+        mTextView3 = (TextView)findViewById(R.id.record3);
+        mTextView4 = (TextView)findViewById(R.id.record4);
+
+
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, URL, null,
                 new Response.Listener<JSONObject>() {
-                    String string;
-
                     @Override
                     public void onResponse(JSONObject response) {
 
-
                         try {
-                            JSONArray jsonArray = response.getJSONArray("throttle");
-                            for (int j = 0; j < jsonArray.length(); j++) {
-
-                                string = jsonArray.getJSONObject(j).getString("limit");
-                                mTextView1.setText(string);
+                            for (int j = 0; j < response.length(); j++)
+                            {
+                                JSONObject throttleJsonData = response.getJSONObject("throttle");
+                                String countString = throttleJsonData.getString("count_remaining");
+                                mTextView1.setText(countString);
+                                String decisionString= throttleJsonData.getString("decision");
+                                mTextView2.setText(decisionString);
+                                String limitString= throttleJsonData.getString("limit");
+                                mTextView3.setText(limitString);
+                                String intervalString= throttleJsonData.getString("interval");
+                                mTextView4.setText(intervalString);
                             }
 
-                            /*ContentValues values = new ContentValues();
+                           /* ContentValues values = new ContentValues();
                             values.put("record_name", string);*/
                         } catch (JSONException e) {
                             e.printStackTrace();
                         }
                     }
                 },
-
-                new Response.ErrorListener() {
-
+                new Response.ErrorListener()
+                {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         mTextView1.setText(error.toString());
